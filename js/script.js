b4d9b70ece6f8f8370188851a48b872015ba96f0
IvyWesselink <55988292+IvyWesselink@users.noreply.github.com>
2020-03-19 21:08:26 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 // this is your custom javascript code
+let xmlns = "http://www.w3.org/2000/svg";
+let svg = document.createElementNS(xmlns, "svg");
+svg.setAttribute("width", 1000);
+svg.setAttribute("height", 1000);
+svg.setAttribute("viewbox", '0 0 1000 1000');
+
+console.log(cities);
+
+for(let i=0;i<cities.length;i++){
+    let latitude = cities[i].latitude;
+    let longitude = cities[i].longitude;
+    let positieX = (~~longitude + 180)*2;
+    let positieY = (90-~~latitude)*2;
+
+
+    let circle = document.createElementNS(xmlns, "circle");
+    circle.setAttribute("cx", positieX);
+    circle.setAttribute("cy", positieY);
+    circle.setAttribute("r", 2);
+    circle.setAttribute("fill", "lightblue");
+
+    svg.appendChild(circle);
+}
+
+document.body.appendChild(svg);
